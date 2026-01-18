b2f095ac16838a91d612aafc356a4a061e7b8c0d
Reyhan Singh <reyhansingh01@gmail.com>
2026-01-18 13:20:51 +0530

    gitig

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+export const firebaseConfig = {
+  apiKey: "REPLACE_WITH_FIREBASE_KEY", // This placeholder will be swapped by Vercel
+  authDomain: "forgecodefirebase.firebaseapp.com",
+  projectId: "forgecodefirebase",
+  storageBucket: "forgecodefirebase.firebasestorage.app",
+  messagingSenderId: "343783791551",
+  appId: "1:343783791551:web:c9887aa925dd8eec1e9f0a",
+  measurementId: "G-JBKJ2YFLHQ"
+};
